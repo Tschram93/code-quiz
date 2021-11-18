8187c41fb940a06b9205d1319260f65eedf10c14
TS93 <tschram93@yahoo.com>
2021-11-18 16:18:24 -0500

    structuring question box with buttons for answer options

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 // Selectors
+const options = document.querySelector('#options');
 const nextBtn = document.querySelector('#nextBtn');
+const questionText = document.querySelector('#questionText');
 const startBtn = document.querySelector('#startBtn');
