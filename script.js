f00b0f9cd312e9cc056a5adbd88e9584fcab15c8
TS93 <tschram93@yahoo.com>
2021-11-18 20:10:53 -0500

    planned script with psuedos

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const options = document.querySelector('#options');
 const nextBtn = document.querySelector('#nextBtn');
 const questionText = document.querySelector('#questionText');
 const startBtn = document.querySelector('#startBtn');
+
+// Functions to:
+
+// startQuiz
+function startQuiz() {
+
+}
+
+// nextQue
+function nextQue() {
+
+}
+
+// Selection an option for the question being asked
+function selectOption() {
+
+}
